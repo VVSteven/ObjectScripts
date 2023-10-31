39e2100df77b8e4783f4951e4193b3453e0663d7
VVSteven <21elvisc@gmail.com>
2023-10-31 11:12:43 -0700

    finished exercize 1

---
+++ label_maker.js
@@ -0,0 +1,11 @@
+const myInfo = { 
+    name: "Elvis", 
+    address: "Main Street St. 123",
+    city: " Pheonix",
+    state: "Arizona",
+    zip: "90210"
+}; printContact(myInfo);
+
+function printContact(){
+    console.table(myInfo);
+}
